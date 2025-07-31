d4c3e21419075619509d7e2b318b64b027b8b7fd
Securelinkcctvms <securelinkcctvms@gmail.com>
2025-07-31 12:34:17 +0500

    First upload

--- lib/waypoints/links.php
+++ lib/waypoints/links.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
   $links = array(
     'js' => 'lib/waypoints/waypoints.min.js'
   );
 ?>
-=======
-<?php
-  $links = array(
-    'js' => 'lib/waypoints/waypoints.min.js'
-  );
-?>
->>>>>>> 1def73f (First upload)
